1d692190d44f6f54601d0a91f97d98ac24301ff5
Daniel Michael Kracht <omegadestroya@gmail.com>
2014-05-31 16:06:00 +0200

    Enabled NPC Command for Everyone. Added Trap variable to test if it fixes unknown command bug.

--- OJRP Source/Enhanced/source/cgame/cg_consolecmds.c
+++ OJRP Source/Enhanced/source/cgame/cg_consolecmds.c
@@ -395,6 +395,7 @@ void CG_InitConsoleCommands( void ) {
 	trap_AddCommand ("teamvote");
 	trap_AddCommand ("stats");
 	trap_AddCommand ("teamtask");
+   trap_AddCommand ("npc"); // Test
 	trap_AddCommand ("loaddefered");	// spelled wrong, but not changing for demo
 	//[MELEE]
 	trap_AddCommand ("togglesaber");
--- OJRP Source/Enhanced/source/game/g_cmds.c
+++ OJRP Source/Enhanced/source/game/g_cmds.c
@@ -4783,7 +4783,7 @@ void ClientCommand( int clientNum ) {
 		Cmd_Notarget_f (ent);
 	else if (Q_stricmp (cmd, "noclip") == 0)
 		Cmd_Noclip_f (ent);
-	else if ( Q_stricmp( cmd, "NPC" ) == 0 && CheatsOk(ent) && !in_camera)
+	else if ( Q_stricmp( cmd, "NPC" ) == 0 /*&& CheatsOk(ent)*/ && !in_camera)
 	{
 		Cmd_NPC_f( ent );
 	}
